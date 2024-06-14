43afd2411e6c3613d3c0a782c4107723d6953d00
Raphael Rodes <lal_ro@outlook.com>
2024-06-14 10:14:53 -0400

    corrigindo a responsividade

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,27 +14,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    $('.gallery__grid').slick({
-        lazyLoad: 'ondemand',
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-});
